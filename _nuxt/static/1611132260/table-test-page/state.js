fc2378b96adf4d3cfd197989ca51f8c02f81a1ba
yurisabbadini <yurisabbadini@users.noreply.github.com>
2021-01-20 08:44:26 +0000

    deploy: wezippit/frontend-src@549e8e285f5cc349368130ed3aa3f2977af3b402

---
+++ _nuxt/static/1611132260/table-test-page/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1611132260",layout:"default",error:null,state:{signInTokens:{},projects:[],customers:[],mockState:{}},serverRendered:true,routePath:"\u002Ftable-test-page",config:{},apollo:{defaultClient:Object.create(null)}};
