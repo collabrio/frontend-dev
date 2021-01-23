a397734aa563259b3e3fa1fb41b32c9d21e8c070
jdies <jdies@users.noreply.github.com>
2021-01-23 10:10:05 +0000

    deploy: wezippit/frontend-src@7b23ebb872beb6bfcec09e7108f0ebf31585b659

--- _nuxt/static/1611171972/overview/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1611171972",layout:"default",error:null,state:{signInTokens:{},projects:[],customers:[],mockState:{}},serverRendered:true,routePath:"\u002Foverview",config:{},apollo:{defaultClient:Object.create(null)}};
